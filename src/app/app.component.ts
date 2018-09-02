37a5cebc2fae986cb09e0273547deb0cdb016607
satwikmvv <satwikmvv@gmail.com>
2018-09-02 01:19:21 -0400

    alpha version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoArray=[]
+  addTodo(todo){
+      this.todoArray.push(todo.value)
+      console.log(this,todo)
+      todo.value=""
+  }
+
+  deleteItem(value) {
+    this.todoArray=this.todoArray.filter(x=>x!==value)
+  }
+
+  todoSubmit(value:any,todoForm:any){
+    if(value.todo!==null && value.todo!==""){
+      this.todoArray.push(todoForm.value.todo)
+      todoForm.resetForm()
+      }else{
+        alert('Field required **')
+      }
+    console.log(value)
+  }
 }
